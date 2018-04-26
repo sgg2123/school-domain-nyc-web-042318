3bb0296a5ac3d5a50bbb8762fb1d8416fb37fbfa
Sydney Garay <sgg2123@columbia.edu>
2018-04-26 02:57:00 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  
+  def add_student(name, grade)
+    result = @roster.key?(grade)
+    if result == true
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    unsorted = @roster[grade]
+    unsorted.sort
+  end
+  
+end
+
+
+# @roster["name"] = []
+#     @roster["name"] << new_value_for_value_array
+#     @roster["grade"] = []
+#     @roster["grade"] << new_value_for_value_array
